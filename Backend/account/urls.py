7d54524ab696241f8b02efaf25ae727c95c0be68
KhaledGassara <118073912+KhaledGs5@users.noreply.github.com>
2024-09-20 10:57:55 +0100

    Delete Backend directory

--- Backend/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import add_user, get_user, update_user, delete_user
-
-urlpatterns = [
-    path('add/', add_user, name='add_user'),
-    path('get/', get_user, name='get_user'),
-    path('put/<int:id>/', update_user, name='update_user'),
-    path('delete/<int:id>/', delete_user, name='delete_user'),
-]
